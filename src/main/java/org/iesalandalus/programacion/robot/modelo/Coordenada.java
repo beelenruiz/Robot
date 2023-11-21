4a326f1a430e4a0d11347424e9fd899659213ae9
beelenruiz <belenrumo2005@gmail.com>
2023-11-21 12:33:39 +0100

    Create Coordenada.java

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Coordenada.java
@@ -0,0 +1,19 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public record Coordenada(int x, int y) {
+    private void validarX(int x){
+        if (x < 0 || x > 9){
+            throw new IllegalArgumentException("No es válido");
+        }
+    }
+    private void validarY(int alto){
+        if (y < 0 || y > 9){
+            throw new IllegalArgumentException("No es válido");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Coordenada -> " + "x = " + x + ", y = " + y;
+    }
+}
