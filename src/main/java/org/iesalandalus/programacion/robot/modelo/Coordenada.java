a77043c0107f4c896a498675eca346bcda82d9eb
beelenruiz <belenrumo2005@gmail.com>
2023-11-23 08:54:01 +0100

    Update Coordenada.java

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Coordenada.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Coordenada.java
@@ -1,5 +1,8 @@
 package org.iesalandalus.programacion.robot.modelo;
 
+import static org.iesalandalus.programacion.robot.modelo.Zona.alto;
+import static org.iesalandalus.programacion.robot.modelo.Zona.ancho;
+
 public record Coordenada(int x, int y) {
     private void validarX(int x){
         if (x < 0 || x > 9){
@@ -10,12 +13,6 @@ public record Coordenada(int x, int y) {
         if (y < 0 || y > 9){
             throw new IllegalArgumentException("No es válido");
         }
-    }
-
-    static Coordenada centro;
-    public void centro(int x, int y){
-        x = 5;
-        y = 5;
     }
 
     @Override
