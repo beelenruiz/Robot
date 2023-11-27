293d1cb20cf03ce359556b6142ebd86b65496abc
beelenruiz <belenrumo2005@gmail.com>
2023-11-27 11:55:51 +0100

    arreglo robot
    Update Robot.java

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Robot.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Robot.java
@@ -1,61 +1,109 @@
 package org.iesalandalus.programacion.robot.modelo;
 
+import javax.naming.OperationNotSupportedException;
 import java.util.Objects;
 
 public class Robot {
-    Zona zona;
-    Orientacion orientacion;
-    Coordenada coordenada;
+    private Zona zona;
+    private Orientacion orientacion;
+    private Coordenada coordenada;
 
     public Robot(){
-        getZona(zona);
+        zona = new Zona();
+        orientacion = Orientacion.NORTE;
+        coordenada = zona.getCentro();
     }
     public Robot(Zona zona){
-        this();
-        this.zona = zona;
+        setZona(zona);
+        orientacion = Orientacion.NORTE;
+        coordenada = zona.getCentro();
     }
     public Robot(Zona zona, Orientacion orientacion){
-        this(zona);
-        this.orientacion = orientacion;
+        setZona(zona);
+        setOrientacion(orientacion);
+        coordenada = zona.getCentro();
     }
     public Robot(Zona zona, Orientacion orientacion, Coordenada coordenada){
-        this(zona, orientacion);
-        this.coordenada = coordenada;
+        setZona(zona);
+        setOrientacion(orientacion);
+        setCoordenada(coordenada);
     }
     public Robot(Robot robot){
-        this();
         Objects.requireNonNull(robot, "No puedo copiar un robot nulo.");
-        robot.zona = zona;
-        robot.coordenada = coordenada;
-        robot.orientacion = orientacion;
+        zona = robot.zona;
+        coordenada = robot.coordenada;
+        orientacion = robot.orientacion;
     }
 
     public Zona getZona() {
         return zona;
     }
     private void setZona(Zona zona){
-        Objects.requireNonNull(zona, "No puedes poner una zona nula");
-        this.zona = zona;
+        this.zona = Objects.requireNonNull(zona, "No puedes poner una zona nula");
     }
 
     public Orientacion getOrientacion() {
         return orientacion;
     }
     private void setOrientacion(Orientacion orientacion) {
-        Objects.requireNonNull(orientacion, "No puedes poner una orientación nula.")
-        this.orientacion = orientacion;
+        this.orientacion = Objects.requireNonNull(orientacion, "No puedes poner una orientación nula.");
     }
 
     public Coordenada getCoordenada() {
         return coordenada;
     }
     private void setCoordenada(Coordenada coordenada) {
-        Objects.requireNonNull(coordenada, "No puedes poner una coordenada nula.")
+        Objects.requireNonNull(coordenada, "No puedes poner una coordenada nula.");
+        if (!zona.pertenece(coordenada)) {
+            throw new IllegalArgumentException("La coordenada no pertenece a la zona");
+        }
         this.coordenada = coordenada;
     }
 
-    public void avanzar(){
-        
+    public void avanzar() throws OperationNotSupportedException {
+        int nuevaX = coordenada.x();
+        int nuevaY = coordenada.y();
+        switch (orientacion){
+            case NORTE -> nuevaY++;
+            case NORESTE -> {nuevaX++; nuevaY++;}
+            case ESTE -> nuevaX++;
+            case SURESTE -> {nuevaX++; nuevaY--;}
+            case SUR -> nuevaY--;
+            case SUROESTE -> {nuevaX--; nuevaY--;}
+            case OESTE -> nuevaX--;
+            case NOROESTE -> {nuevaX--; nuevaY++;}
+        }
+        try {
+            setCoordenada(new Coordenada(nuevaX, nuevaY));
+        } catch (IllegalArgumentException e){
+            throw new OperationNotSupportedException("No se puede avanzar, se sale de la zona");
+        }
+    }
+
+    public void girarALaIzquierda() {
+        orientacion = switch (orientacion) {
+            case NORTE -> Orientacion.NOROESTE;
+            case NORESTE -> Orientacion.NORTE;
+            case ESTE -> Orientacion.NORESTE;
+            case SURESTE -> Orientacion.ESTE;
+            case SUR -> Orientacion.SURESTE;
+            case SUROESTE -> Orientacion.SUR;
+            case OESTE -> Orientacion.SUROESTE;
+            case NOROESTE -> Orientacion.OESTE;
+        };
+    }
+
+    public void girarALaDerecha() {
+        orientacion = switch (orientacion) {
+            case NORTE -> Orientacion.NORESTE;
+            case NORESTE -> Orientacion.ESTE;
+            case ESTE -> Orientacion.SURESTE;
+            case SURESTE -> Orientacion.SUR;
+            case SUR -> Orientacion.SUROESTE;
+            case SUROESTE -> Orientacion.OESTE;
+            case OESTE -> Orientacion.NOROESTE;
+            case NOROESTE -> Orientacion.NORTE;
+        };
     }
 
     @Override
